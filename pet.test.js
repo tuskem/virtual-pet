f03c30a3a50a0c0111e0ac57333403099052eaaa
tuskem <ma.edwards@live.com>
2018-09-12 14:14:01 +0100

    Adding to virtual tests

--- pet.test.js
+++ pet.test.js
@@ -3,7 +3,27 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(new Pet('Casper')).toBeInstanceOf(Object);
     });
   });
-  
+
+  describe('constructor', () => {
+      it('sets the name property', () => {
+          const pet = new Pet('Casper');
+          expect(pet.name). toEqual('Casper');
+      });
+
+
+
+
+
+      
+      describe('growUp', () => {
+        it('increments the age by 1 when called', () => {
+          const pet = new Pet('Casper');
+          pet.growUp();
+          expect(pet.age).toEqual(1);
+        });
+      });
+      
+    
